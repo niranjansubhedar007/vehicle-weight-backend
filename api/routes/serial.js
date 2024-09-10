8451d03c9a991f00b1019cc057495694eef31d11
Niranjan Subhedar <niranjansubhedar@gmail.com>
2024-09-10 10:49:05 +0530

    added serial port path for the ubntu

--- api/routes/serial.js
+++ api/routes/serial.js
@@ -13,7 +13,7 @@ const { Router } = require('express')
 const router = express.Router()
 
 const serialPort = new SerialPort({ 
-    path: 'COM1',
+    path: '/dev/ttyUSB0',
     baudRate: 2400 ,
 })
 const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\r\n' }))
